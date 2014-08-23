3e6b59e71bea999ff53c8758dedf1f6b03647e56
Giancarlo Daniele <giancarlo@cs.stanford.edu>
2014-08-23 08:47:19 -0700

    stuff on screen

--- FitPlus/CameraViewController.swift
+++ FitPlus/CameraViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class CameraViewController: UIViewController {
 
     @IBOutlet weak var cameraView: UIView!
+    @IBOutlet weak var snapButton: UIButton!
+    @IBOutlet weak var flipButton: UIButton!
+    @IBOutlet weak var timeLimitButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,5 +29,18 @@ class CameraViewController: UIViewController {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
+    
+    override func viewDidAppear(animated: Bool) {
+        setupCameraView()
+    }
+    
+//    MARK: UI Positioning helpers
 
+//    Sets auto layout attributes of cameraView UI elements
+    func setupCameraView() {
+        cameraView.frame = UIScreen.mainScreen().applicationFrame
+        timeLimitButton.frame = CGRectMake(0, 0, timeLimitButton.frame.size.width, timeLimitButton.frame.size.height)
+        snapButton.frame = CGRectMake(0, 0, 0, 0)
+        flipButton.frame = CGRectMake(200, 200, flipButton.frame.size.width, flipButton.frame.size.height)
+    }
 }
