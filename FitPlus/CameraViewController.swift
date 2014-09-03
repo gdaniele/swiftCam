c7dda00ea08146dcb8177e6968eb9e12b19f984d
Giancarlo Daniele <giancarlo@cs.stanford.edu>
2014-09-03 06:25:14 -0700

    camera can be authorized

--- FitPlus/CameraViewController.swift
+++ FitPlus/CameraViewController.swift
@@ -7,16 +7,59 @@
 //
 
 import UIKit
+import AVFoundation
+import AssetsLibrary
 
 class CameraViewController: UIViewController {
-
     @IBOutlet weak var cameraView: UIView!
     @IBOutlet weak var snapButton: UIButton!
     @IBOutlet weak var flipButton: UIButton!
     @IBOutlet weak var timeLimitButton: UIButton!
     
+    var sessionQueue : dispatch_queue_t?
+    var session : AVCaptureSession?
+    var videoDeviceInput : AVCaptureDeviceInput?
+    var videoDevice : AVCaptureDevice?
+    var movieFileOutput : AVCaptureMovieFileOutput?
+    var stillImageOutput : AVCaptureStillImageOutput?
+    var backgroundRecordingId : UIBackgroundTaskIdentifier?
+    var deviceAuthorized : Bool?
+    
+    let CONTROL_NORMAL_COLOR : UIColor?
+    let CONTROL_HIGHLIGHT_COLOR : UIColor?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.snapButton.layer.cornerRadius = 4
+        self.flipButton.layer.cornerRadius = 4
+        self.timeLimitButton.layer.cornerRadius = 4
+        
+        self.snapButton.clipsToBounds = true
+        self.flipButton.clipsToBounds = true
+        self.timeLimitButton.clipsToBounds = true
+        
+//        Create the AV Session!
+        var session : AVCaptureSession = AVCaptureSession.alloc()
+        self.session = session
+        
+//        TODO : Set up preview
+        
+        self.checkForAuthorizationStatus()
+        
+//        It's not safe to mutate an AVCaptureSession from multiple threads at the same time. Here, we're creating a sessionQueue so that the main thread is not blocked when AVCaptureSetting.startRunning is called.
+        var queue : dispatch_queue_t = dispatch_queue_create("sesion queue", DISPATCH_QUEUE_SERIAL);
+        self.sessionQueue = queue
+        
+        dispatch_async(queue, { () -> Void in
+            self.backgroundRecordingId = UIBackgroundTaskInvalid
+            var error : NSError?
+                        
+            
+        })
+        
+
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,13 +74,26 @@ class CameraViewController: UIViewController {
     }
     
     override func viewDidAppear(animated: Bool) {
-        setupCameraView()
+        
     }
     
-//    MARK: UI Positioning helpers
-
-//    Sets auto layout attributes of cameraView UI elements
-    func setupCameraView() {
-        cameraView.frame = UIScreen.mainScreen().applicationFrame
+    func checkForAuthorizationStatus() {
+        var mediaType : NSString = AVMediaTypeVideo;
+        AVCaptureDevice .requestAccessForMediaType(mediaType, completionHandler: { (granted) -> Void in
+            if (granted) {
+                self.deviceAuthorized = true
+            } else {
+                dispatch_sync(dispatch_get_main_queue(), { () -> Void in
+                    UIAlertView(title: "FitPlus", message: "FitPlus doesn't have permission to use the camera!", delegate: self, cancelButtonTitle: "OK").show()
+                    self.deviceAuthorized = false
+                })
+            }
+        })
     }
+    
+//    MARK : Utilities
+    class func deviceWithMediaTypeAndPosition(mediaType: NSString, position: AVCaptureDevicePosition) -> AVCaptureDevice {
+        return AVCaptureDevice.alloc();
+    }
+    
 }
